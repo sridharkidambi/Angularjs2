bb5f21f259e6aae6b69ac260e206f0b02bfad084
Sridhar Kidambi <sridharkidambi@Srinivasas-MacBook-Pro.local>
2016-09-18 16:27:16 -0500

    fourth commit with Directives completion.

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,9 +1,10 @@
 import { Component} from '@angular/core';
-
+import { DropdownDirective } from './dropdown.directive';
 @Component({
   moduleId: module.id,
   selector: 'rb-header',
-  templateUrl: 'header.component.html'
+  templateUrl: 'header.component.html',
+  directives:[ DropdownDirective ]
 })
 export class HeaderComponent  {
 
